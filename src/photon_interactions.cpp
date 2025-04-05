f292f153869a21e23c25f283ff4a53a9e8971e7f
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-04-05 20:46:00 +0100

    Added photoelectric and compton effects.

--- src/photon_interactions.cpp
+++ src/photon_interactions.cpp
@@ -5,8 +5,37 @@
 #include<memory>
 #include<stdexcept>
 
-double photoelectricEffect(){}
+double photoelectricEffect(const std::shared_ptr<Photon>& photon)
+{
+  if (!photon)
+  {
+    std::cerr << "Error: Null photon pointer in photoelectric effect" << std::endl;
+    return 0.0;
+  }
+  double energy = photon->getEnergy();
+  std::cout << "Photoelectric effectr: Photon of energy " << energy
+            << " MeV is absorved completely" << std::endl;
+  
+  return energy;
+}
 
-double comptonEffect(){}
+double comptonEffect(const std::shared_ptr<Photon>& photon, double theta)
+{
+  if (!photon)
+  {
+    std::cerr << "Error: Null photon pointer in photoelectric effect" << std::endl;
+    return 0.0;
+  }
+
+  const double electronMass = 0.511; // in MeV/c^2 with c=1
+  double initialEnergy = photon->getEnergy();
+
+  // Compton scattering formula
+  double newEnergy = initialEnergy / (1 + (initialEnergy / electronMass) * (1 - std::cos(theta)));
+
+  std::cout << "Compton Effect: Photon energy changed from " << initialEnergy
+            << " MeV to " << newEnergy << " MeV, with collision angle = " << theta << " rad" << std::endl;
+
+}
 
 std::vector<std::shared_ptr<Electron>> pairProduction(){}
