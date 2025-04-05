e49b75e95405849c827c9a0cb22025c380e4b509
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-04-05 20:45:05 +0100

    Created the 3 interactions of photons. Need to add the formulas and outputs.

---
+++ src/photon_interactions.cpp
@@ -0,0 +1,12 @@
+#include"photon.h"
+#include"electron.h"
+#include<iostream>
+#include<cmath>
+#include<memory>
+#include<stdexcept>
+
+double photoelectricEffect(){}
+
+double comptonEffect(){}
+
+std::vector<std::shared_ptr<Electron>> pairProduction(){}
